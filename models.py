9c4a314f61543244823603c89ca9ce8ccbb38aab
Furkan <furkancilogluuu@gmail.com>
2025-05-30 01:22:02 +0300

    Set backend sections and add admin panel.

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +15,14 @@ class Post(db.Model):
 
     def __repr__(self):
         return f"<Post {self.title}>"
+
+class AdminUser(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
